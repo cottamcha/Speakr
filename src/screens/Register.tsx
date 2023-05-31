9c1ead2ba363f4e20ab0e1c1e00ab21209f61ad6
Jake Cottam <jacobcottam@gmail.com>
2023-05-31 13:32:59 +0100

    fix: turn autocompete off for email

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -82,6 +82,7 @@ const RegisterScreen:Component = () => {
                           name="email"
                           id="email"
                           class="mt-1 block w-full rounded-lg border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                          autocomplete="off"
                         />
                         <FormError>{errors["email"]}</FormError>
                       </div>
